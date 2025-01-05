775ebfd72a8ca0912ce68eecc69aee6d417d2005
unknown <965447695@qq.com>
2025-01-05 11:20:48 +0800

    delete

--- dogeconin/src/config/contract.ts
+++
@@ -1,6 +0,0 @@
-import ContractABI from '../contracts/Contract.json';
-
-export const CONTRACT_ADDRESS = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9";
-export const CONTRACT_ABI = ContractABI.abi; 
-
-
